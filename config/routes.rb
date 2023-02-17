1a631c63bb7eaf22bce7c6a2abf528a58eb03401
825814 <120297399+825814@users.noreply.github.com>
2023-02-17 00:45:14 +0000

    [add]ログインとサインアップ　ルーチング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  devise_for :admins, skip: [:passwords, :registrations], controllers: {
+    sessions: "admin/sessions"
+  }
+
   root to: 'homes#top'
   get '/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
