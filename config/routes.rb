fb3b07a7d3d28af75c1f1befef06e48e47f0c427
825814 <120297399+825814@users.noreply.github.com>
2023-02-17 01:47:26 +0000

    [add]コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
-  root to: 'homes#top'
-  get '/about' => 'homes#about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
