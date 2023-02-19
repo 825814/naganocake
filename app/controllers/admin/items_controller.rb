88d6792a0f3dfe2ffcd07eacf2511d2bd441aa43
825814 <120297399+825814@users.noreply.github.com>
2023-02-19 12:28:40 +0000

    [add]admin商品一覧画面作成中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,13 +3,17 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
     @genres = Genre.all
-    
+
   end
 
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_item_path
+    redirect_to admin_item_path(@item)
+  end
+
+  def index
+    @items = Item.all
   end
 
   private
