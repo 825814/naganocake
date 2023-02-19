3abe7a2dbfcea134a1ef01c975201d4fac723aab
825814 <120297399+825814@users.noreply.github.com>
2023-02-19 07:35:40 +0000

    [add]admin商品新規登録画面作成中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,23 @@
 class Admin::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+    @genres = Genre.all
+    
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active)
+  end
+
+
+
 end
