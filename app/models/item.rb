a0b6d408c566beb0d83478ef18ebe4cedc21052d
825814 <120297399+825814@users.noreply.github.com>
2023-02-23 12:09:04 +0000

    [add]admin注文履歴詳細画面作成中

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  has_many :order_details, dependent: :destroy
   belongs_to :genre
 
   def get_image(width, height)
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  has_many :order_details, dependent: :destroy
+  belongs_to :customer
+
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 配送準備中: 3, 配送済み: 4}
 end
