580b811be5eeea548c87880a7a54c6980ba38573
825814 <120297399+825814@users.noreply.github.com>
2023-03-08 14:31:59 +0000

    [update]cart_itemのcreateアクション

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,36 +5,38 @@ class Public::CartItemsController < ApplicationController
 
   def index
      @cart_items = current_customer.cart_items
+     @total = 0
 
   end
 
   def create
-      @cart_item = CartItem.new(params_cart_item)
-      @cart_item.customer_id=current_customer.id
-      @cart_items=current_customer.cart_items.all
-      
-        @cart_items.each do |cart_item|
-          if cart_item.item_id==@cart_item.item_id
-          new_amount = cart_item.amount + @cart_item.amount
-          cart_item.update_attribute(:amount, new_amount)
-          @cart_item.delete
-          end
-        end
 
-        @cart_item.save
-        redirect_to cart_items_path,notice:"カートに商品が入りました"
+    # @item = Item.find(cart_item_params[:item_id])
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+
+    cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    sum = cart_item.amount + params[:cart_item][:amount].to_i
+    cart_item.update(amount: sum)
+
+
+    else
+    cart_item = CartItem.new(params_cart_item)
+    # binding.pry
+    cart_item.save
+
+    end
+    redirect_to cart_items_path
   end
 
   def destroy
     cart_item = CartItem.find(params[:id])
-    if cart_item.customer_id = current_customer.id
-    cart_item.destroy
+    if cart_item.destroy
     redirect_to cart_items_path
     end
   end
 
   def destroy_all
-    customer = current_customer
+    customer = current_customer.id
     customer.cart_items.destroy_all
     redirect_to cart_items_path
   end
