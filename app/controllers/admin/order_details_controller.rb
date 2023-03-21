61359fac8ebe00525343bb55eb69f39272f3c38b
825814 <120297399+825814@users.noreply.github.com>
2023-03-21 12:38:33 +0000

    [update]注文履歴

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,3 +1,17 @@
 class Admin::OrderDetailsController < ApplicationController
   
+  def show
+    @order_details = OrderDetail.find(params[:id])
+  end
+  
+  def update
+    order_detail = OrderDetail.find(params[:id])
+    order_detail.update(order_detail_params)
+    redirect_to admin_order_path(order_detail.id)
+  end
+  
+  private
+  
+    def order_detail_params
+      params.require(:order_detail).permit(:order_id, :item_id, :price, :amount, :making_status)
 end
