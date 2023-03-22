c1f0a80103260ac5de24d069a8e31568b0371ab2
825814 <120297399+825814@users.noreply.github.com>
2023-03-22 13:17:11 +0000

    [update]ヘッダー修正中

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,8 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 配送準備中: 3, 配送済み: 4}
 
-
+  def subtotal
+    price * amount
+  end
 
 end
