ef3a37c980e332d4b4dd50fd2a3df52b9399b9c8
825814 <120297399+825814@users.noreply.github.com>
2023-03-24 10:18:30 +0000

    [update]退会機能

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,10 @@ class Customer < ApplicationRecord
   # belongs_to :cart_item
   has_many :cart_items, dependent: :destroy
 
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
