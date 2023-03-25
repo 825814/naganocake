5edbe2c17ceb3890acec17ae266ace10393b4693
825814 <120297399+825814@users.noreply.github.com>
2023-03-25 02:26:23 +0000

    [add]ステータスの英単語

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,10 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 配送準備中: 3, 配送済み: 4}
+  #enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 配送準備中: 3, 配送済み: 4}
+  enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4}
 
+ 
   def subtotal
     price * amount
   end
